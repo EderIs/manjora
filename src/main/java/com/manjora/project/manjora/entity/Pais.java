faef912313778bb88bbcb01fb9662a5ed2c08652
Sebastian <villagrans131799@gmail.com>
2021-01-19 15:07:24 -0600

    Cambios paises.
    relaciones.

--- src/main/java/com/manjora/project/manjora/entity/Pais.java
+++ src/main/java/com/manjora/project/manjora/entity/Pais.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -14,6 +15,9 @@ public class Pais {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombrePais;
+	
+	//@OneToOne(mappedBy = "paises")
+	//private Estado estado;
 	
 	public Pais() {
 
