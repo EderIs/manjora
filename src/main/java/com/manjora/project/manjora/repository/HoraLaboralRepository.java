ff6226557ebbffce3dd7dd596849343c6eaff268
Alejandro Rodriguez <alejandro.rodriguezh1879@gmail.com>
2021-01-29 10:11:05 -0600

    Arreglos de empleado
    Arreglos a empleado routing

--- src/main/java/com/manjora/project/manjora/repository/HoraLaboralRepository.java
+++ src/main/java/com/manjora/project/manjora/repository/HoraLaboralRepository.java
@@ -1,16 +1,21 @@
 package com.manjora.project.manjora.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.manjora.project.manjora.entity.Banco;
 import com.manjora.project.manjora.entity.HoraLaboral;
 import com.manjora.project.manjora.entity.HorariosDeTrabajo;
 
 @Repository
 public interface HoraLaboralRepository extends JpaRepository<HoraLaboral, Long>{
 
+	@Query(value="select * from horas_laborales where nombre_horal LIKE %?1%",nativeQuery = true)
+	List<HoraLaboral>findAllByNombre(String nombreHoraL);
 	Optional<HoraLaboral> findByNombreHoraL(String nombreHoraL);
 	boolean existsByNombreHoraL(String nombreHoraL);
 	
