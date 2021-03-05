4064c19a9ba631da50a2564bda44c8df6954f9f0
Sebastian <villagrans131799@gmail.com>
2021-03-04 18:39:40 -0600

    Cambios Actividad back

--- src/main/java/com/manjora/project/manjora/controller/ActividadController.java
+++ src/main/java/com/manjora/project/manjora/controller/ActividadController.java
@@ -1,14 +1,20 @@
 package com.manjora.project.manjora.controller;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,20 +49,65 @@ public class ActividadController {
 		
 	try {
 		
-		Actividad actividad = new Actividad(Long.parseLong("0"),actividadDto.getActividad(), 
+		Actividad actividad = new Actividad(actividadDto.getActividad(), 
 				actividadDto.getResumen(),actividadDto.getFechaInicio(),actividadDto.getFechaFinal(),
 				actividadDto.getUsuario(),actividadDto.getEstadoT(),actividadDto.getTarea());
 		
 		this.actividadService.saveActividad(actividad);
 		
-		emilService.sendEmail(actividad.getUsuario().getEmail(), actividad.getActividad(),actividad.getResumen());
+		if(!actividad.getEstadoT()) {
+			emilService.sendEmail(actividad.getUsuario().getEmail(), actividad.getActividad(),actividad.getResumen());	
+		}
 		
-		return new ResponseEntity(new Mensaje("Actividad guardada"),HttpStatus.ACCEPTED);
+		return new ResponseEntity(actividad,HttpStatus.ACCEPTED);
 		
 	}catch(Exception io) {
 		return new ResponseEntity(new Mensaje("No se guardo"),HttpStatus.BAD_REQUEST);
 	}
 		
 	}
+	
+	@PutMapping("/updateA/{id}")
+	public ResponseEntity<?>updateActividad(@PathVariable(name="id")Long idActividad,@RequestBody ActividadDto actividadDto){
+		try {
+			
+			Actividad actividad = this.actividadService.getActividad(idActividad);
+			
+			actividad.setEstadoT(actividadDto.getEstadoT());
+			
+			Date d = new Date();
+			
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss");
+			String strDate= formatter.format(d);
+			
+			actividad.setFechaEntrega(strDate);
+			
+			this.actividadService.saveActividad(actividad);
+			
+			return new ResponseEntity(actividad,HttpStatus.ACCEPTED);
+			
+		}catch(Exception io) {
+			return new ResponseEntity(new Mensaje("Ocurrio un problema"+io.getMessage()),HttpStatus.BAD_REQUEST);
+		}
+		
+	}
+	
+	@DeleteMapping("delete/{id}")
+	public ResponseEntity<?>deleteActividad(@PathVariable(name="id")Long idActividad ){
+		
+		try {
+			
+			Actividad actividad = this.actividadService.getActividad(idActividad);
+			
+			this.actividadService.deleteActividad(actividad);
+			
+			return new ResponseEntity(null,HttpStatus.ACCEPTED);
+		}catch(Exception io) {
+			return new ResponseEntity(new Mensaje("Ocurrio un problema"),HttpStatus.BAD_REQUEST);
+		}
+		
+		
+	}
+	
 	
 }
